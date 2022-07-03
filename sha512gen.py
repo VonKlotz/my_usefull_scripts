127d5de541d85ba45c1b0eed6178d1cd177bd310
VonKlotz <97737377+VonKlotz@users.noreply.github.com>
2022-07-03 18:20:41 +0100

    Update sha512gen.py

--- sha512gen.py
+++ sha512gen.py
@@ -3,24 +3,3 @@ import crypt
 
 password = input("Enter password to hash => ")
 print(crypt.crypt(str(password), crypt.mksalt(crypt.METHOD_SHA512)))
-
-
-
-
-'''
-old overcomplicated version version with args
-import crypt
-import argparse
-
-parser = argparse.ArgumentParser(description='A SHA512 password hasher.')
-parser.add_argument("-p", "--password", help="Prints the supplied argument.", nargs='*')
-arguments = parser.parse_args()
-pstr = arguments.password 
-
-print(str(pstr))
-
-print(crypt.crypt(str(pstr), crypt.mksalt(crypt.METHOD_SHA512)))
-
-
-
-'''
